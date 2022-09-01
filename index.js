dcd32d6da577d683bb0d0c0f1a5a43a3e666fc15
henriquesdias <henrique.dias1915@gmail.com>
2022-09-01 02:20:27 -0300

    .env file has been created. Database connected to node

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => {
+  db = mongoClient.db("API-bate-papo");
+});
 
 server.post("/participants", (req, res) => {
   const { name } = req.body;
