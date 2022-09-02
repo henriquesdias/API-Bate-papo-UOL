2c3964094f1c9efc58d6e903e6f8cc5609e55cde
henriquesdias <henrique.dias1915@gmail.com>
2022-09-02 02:12:47 -0300

    on route '/messages', using method post, the message from user is stored on database. The status 201 is returned

--- index.js
+++ index.js
@@ -20,23 +20,27 @@ mongoClient.connect().then(() => {
 const participantSchema = joi.object({
   name: joi.string().required(),
 });
-
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required(),
+});
 server.post("/participants", async (req, res) => {
   const { name } = req.body;
-  const validation = participantSchema.validate({ name: name });
+  const validation = participantSchema.validate({ name });
   if (validation.error) {
     return res.sendStatus(422);
   }
   try {
     const participants = await db
       .collection("participants")
-      .find({ name: name })
+      .find({ name })
       .toArray();
     if (participants.length !== 0) {
       return res.status(409).send("Usuário já cadastrado");
     } else {
       db.collection("participants").insertOne({
-        name: name,
+        name,
         lastStatus: Date.now(),
       });
       db.collection("messages").insertOne({
@@ -44,7 +48,7 @@ server.post("/participants", async (req, res) => {
         to: "Todos",
         text: "entra na sala...",
         type: "status",
-        time: dayjs().format("hh:mm:ss"),
+        time: dayjs().format("HH:mm:ss"),
       });
       res.sendStatus(201);
     }
@@ -61,9 +65,32 @@ server.get("/participants", async (req, res) => {
     res.sendStatus(500);
   }
 });
-server.post("/messages", (req, res) => {
+server.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
   const { user } = req.headers;
+  const validation = messageSchema.validate({ to, text, type });
+  if (validation.error) {
+    return res.sendStatus(422);
+  }
+  try {
+    const participants = await db
+      .collection("participants")
+      .find({ name: user })
+      .toArray();
+    if (participants.length === 0) {
+      return res.status(422).send("Usuário não encontrado");
+    }
+    db.collection("messages").insertOne({
+      from: user,
+      to,
+      text,
+      type,
+      time: dayjs().format("HH:mm:ss"),
+    });
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 server.get("/messages", (req, res) => {
   const limit = req.query.limit;
